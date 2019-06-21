866aaeb27ba59f30a95dab5c14561e822aa14e81
Ulises Jeremias Cornejo Fandos <ulisescf.24@gmail.com>
2019-06-20 23:43:36 -0300

    Update run_train_all.py

--- src/proto_net/scripts/train/run_train_all.py
+++ src/proto_net/scripts/train/run_train_all.py
@@ -4,10 +4,9 @@ from train_setup import train
 
 configs = {
     'lsa16': {
-        'data.train_way': [10, 18, 20],
-        'data.train_support_query': [(1, 1), (5, 5), (1, 29), (5,25)],
-        'data.test_way': [5, 10],
-        'data.test_support_query': [(1, 1), (5, 5), (1, 15), (5, 11)],
+        'data.train_way': [5, 17],
+        'data.test_way': [5],
+        'data.support_query': [(1, 1, 1, 1), (1, 15, 1, 1), (1, 29, 1, 1), (1, 1, 1, 15), (5, 5, 5, 5), (5, 25, 5, 5), (5, 5, 5, 11)],
 
         'data.rotation_range': [0, 5, 25], 
         'data.width_shift_range': [0, 0.2],
@@ -19,10 +18,9 @@ configs = {
         'train.lr': [0.001]
     },
     'rwth': {
-        'data.train_way': [10, 18, 20],
-        'data.train_support_query': [(1, 1), (5, 5), (1, 19), (5, 15)],
-        'data.test_way': [5, 10],
-        'data.test_support_query': [(1, 1), (5, 5), (1, 9)],
+        'data.train_way': [5, 18],
+        'data.test_way': [5],
+        'data.support_query': [(1, 1, 1, 1), (1, 19, 1, 1), (1, 1, 1, 9), (5, 5, 5, 5), (5, 15, 5, 5)],
 
         'data.rotation_range': [0, 5, 25], 
         'data.width_shift_range': [0, 0.2], 
@@ -61,32 +59,31 @@ for dataset in ['lsa16', 'rwth']:
     ds_config = configs[dataset]
 
     for train_way in ds_config['data.train_way']:
-        for train_support, train_query in ds_config['data.train_support_query']:
-            for test_way in ds_config['data.test_way']:
-                for test_support, test_query in ds_config['data.test_support_query']:
-                    for rotation_range in ds_config['data.rotation_range']:
-                        for width_shift_range in ds_config['data.width_shift_range']:
-                            for height_shift_range in ds_config['data.height_shift_range']:
-                                for horizontal_flip in ds_config['data.horizontal_flip']:
-                                    for model_type in ds_config['model.type']:
-                                        for lr in ds_config['train.lr']:
-                                            custom_params = {
-                                                'data.train_way': train_way,
-                                                'data.train_support': train_support,
-                                                'data.train_query': train_query,
-                                                'data.test_way': test_way,
-                                                'data.test_support': test_support,
-                                                'data.test_query': test_query,
+        for test_way in ds_config['data.test_way']:
+            for train_support, train_query, test_support, test_query in ds_config['data.support_query']:
+                for rotation_range in ds_config['data.rotation_range']:
+                    for width_shift_range in ds_config['data.width_shift_range']:
+                        for height_shift_range in ds_config['data.height_shift_range']:
+                            for horizontal_flip in ds_config['data.horizontal_flip']:
+                                for model_type in ds_config['model.type']:
+                                    for lr in ds_config['train.lr']:
+                                        custom_params = {
+                                            'data.train_way': train_way,
+                                            'data.train_support': train_support,
+                                            'data.train_query': train_query,
+                                            'data.test_way': test_way,
+                                            'data.test_support': test_support,
+                                            'data.test_query': test_query,
 
-                                                'data.rotation_range': rotation_range, 
-                                                'data.width_shift_range': width_shift_range,
-                                                'data.height_shift_range': height_shift_range,
-                                                'data.horizontal_flip': horizontal_flip, 
+                                            'data.rotation_range': rotation_range, 
+                                            'data.width_shift_range': width_shift_range,
+                                            'data.height_shift_range': height_shift_range,
+                                            'data.horizontal_flip': horizontal_flip, 
 
-                                                'model.type': model_type,
+                                            'model.type': model_type,
 
-                                                'train.lr': lr
-                                            }
+                                            'train.lr': lr
+                                        }
 
-                                            preprocessed_config = preprocess_config({ **config_from_file['TRAIN'], **custom_params })
-                                            train(preprocessed_config)
+                                        preprocessed_config = preprocess_config({ **config_from_file['TRAIN'], **custom_params })
+                                        train(preprocessed_config)
