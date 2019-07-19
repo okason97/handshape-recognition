a02fd6c5d4a07b711a88f8a465b3978a31fc6bf9
Ulises Jeremias Cornejo Fandos <ulisescf.24@gmail.com>
2019-07-19 08:22:55 -0300

    Update run_train_all.py

--- src/proto_net/scripts/train/run_train_all.py
+++ src/proto_net/scripts/train/run_train_all.py
@@ -7,52 +7,58 @@ configs = {
         'data.train_way': [5, 16],
         'data.test_way': [5],
         # done (1, 1, 1, 1) in previous experiments
-        'data.support_query': [(1, 15, 1, 1), (5, 5, 5, 5), (5, 5, 5, 11), (5, 25, 5, 5)],
+        'data.support_query': [(5, 5, 5, 5)],
+        'data.train_size': [67, 50, 33],
+        'data.test_size': [33],
 
         #'data.rotation_range': [0, 25], 
         #'data.width_shift_range': [0.1], 
         #'data.height_shift_range': [0.1],
         #'data.horizontal_flip': [True, False],
-        'data.args': [(0, 0, 0, False), (24, 0.2, 0.2, True)],
+        'data.args': [(0, 0, 0, False), (10, 0.2, 0.2, True)],
 
         'model.type': ['expr'],
-        'model.nb_layers': [4, 8],
-        'model.nb_filters': [128, 64],
+        'model.nb_layers': [4],
+        'model.nb_filters': [64],
 
         'train.lr': [0.001]
     },
     'ciarp': {
-        'data.train_way': [5, 1],
-        'data.test_way': [5, 1],
+        'data.train_way': [5, 10],
+        'data.test_way': [5],
         # done (1, 1, 1, 1) in previous experiments
-        'data.support_query': [(1, 1, 1, 1), (5, 5, 5, 5)],
+        'data.support_query': [(5, 5, 5, 5)],
+        'data.train_size': [67, 50, 33],
+        'data.test_size': [33],
 
         #'data.rotation_range': [0, 25], 
         #'data.width_shift_range': [0.1], 
         #'data.height_shift_range': [0.1],
         #'data.horizontal_flip': [True, False],
-        'data.args': [(0, 0, 0, False), (24, 0.2, 0.2, True)],
+        'data.args': [(0, 0, 0, False), (10, 0.2, 0.2, True)],
 
         'model.type': ['expr'],
-        'model.nb_layers': [4, 8],
-        'model.nb_filters': [128, 64],
+        'model.nb_layers': [4],
+        'model.nb_filters': [64],
 
         'train.lr': [0.001]
     },
     'rwth': {
-        'data.train_way': [5, 20],
+        'data.train_way': [5, 40],
         'data.test_way': [5],
-        'data.support_query': [(5, 5, 5, 5), (5, 15, 5, 5)],
+        'data.support_query': [(5, 5, 5, 5)],
+        'data.train_size': [67, 50, 33],
+        'data.test_size': [33],
         
         #'data.rotation_range': [0, 25], 
         #'data.width_shift_range': [0.1], 
         #'data.height_shift_range': [0.1],
         #'data.horizontal_flip': [True, False],
-        'data.args': [(0, 0, 0, False), (24, 0.2, 0.2, True)],
+        'data.args': [(0, 0, 0, False), (10, 0.2, 0.2, True)],
 
         'model.type': ['expr'],
-        'model.nb_layers': [4, 8],
-        'model.nb_filters': [128, 64],
+        'model.nb_layers': [4],
+        'model.nb_filters': [64],
 
         'train.lr': [0.001]
     }
@@ -83,36 +89,40 @@ for dataset in ['ciarp']:
 
     ds_config = configs[dataset]
 
-    for train_way in ds_config['data.train_way']:
-        for test_way in ds_config['data.test_way']:
-            for train_support, train_query, test_support, test_query in ds_config['data.support_query']:
-                for rotation_range, width_shift_range, height_shift_range, horizontal_flip in ds_config['data.args']:
-                    for model_type in ds_config['model.type']:
-                        for nb_layers in ds_config['model.nb_layers']:
-                            for nb_filters in ds_config['model.nb_filters']:
-                                for lr in ds_config['train.lr']:
-                                    try:
-                                        custom_params = {
-                                            'data.train_way': train_way,
-                                            'data.train_support': train_support,
-                                            'data.train_query': train_query,
-                                            'data.test_way': test_way,
-                                            'data.test_support': test_support,
-                                            'data.test_query': test_query,
+    for train_size in ds_config['data.train_size']:
+        for test_size in ds_config['data.test_size']:
+            for train_way in ds_config['data.train_way']:
+                for test_way in ds_config['data.test_way']:
+                    for train_support, train_query, test_support, test_query in ds_config['data.support_query']:
+                        for rotation_range, width_shift_range, height_shift_range, horizontal_flip in ds_config['data.args']:
+                            for model_type in ds_config['model.type']:
+                                for nb_layers in ds_config['model.nb_layers']:
+                                    for nb_filters in ds_config['model.nb_filters']:
+                                        for lr in ds_config['train.lr']:
+                                            try:
+                                                custom_params = {
+                                                    'data.train_way': train_way,
+                                                    'data.train_support': train_support,
+                                                    'data.train_query': train_query,
+                                                    'data.test_way': test_way,
+                                                    'data.test_support': test_support,
+                                                    'data.test_query': test_query,
+                                                    'data.train_size': train_size,
+                                                    'data.test_size': test_size,
 
-                                            'data.rotation_range': rotation_range, 
-                                            'data.width_shift_range': width_shift_range,
-                                            'data.height_shift_range': height_shift_range,
-                                            'data.horizontal_flip': horizontal_flip, 
+                                                    'data.rotation_range': rotation_range, 
+                                                    'data.width_shift_range': width_shift_range,
+                                                    'data.height_shift_range': height_shift_range,
+                                                    'data.horizontal_flip': horizontal_flip, 
 
-                                            'model.type': model_type,
-                                            'model.nb_layers': nb_layers,
-                                            'model.nb_filters': nb_filters,
+                                                    'model.type': model_type,
+                                                    'model.nb_layers': nb_layers,
+                                                    'model.nb_filters': nb_filters,
 
-                                            'train.lr': lr
-                                        }
+                                                    'train.lr': lr
+                                                }
 
-                                        preprocessed_config = preprocess_config({ **config_from_file['TRAIN'], **custom_params })
-                                        train(preprocessed_config)
-                                    except:
-                                        print("Error. Probably memory :c")
+                                                preprocessed_config = preprocess_config({ **config_from_file['TRAIN'], **custom_params })
+                                                train(preprocessed_config)
+                                            except:
+                                                print("Error. Probably memory :c")
