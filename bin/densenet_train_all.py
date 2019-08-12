951f05a914c633925b2e3c084b8db0708178bc3a
Ulises Jeremias Cornejo Fandos <ulisescf.24@gmail.com>
2019-08-12 16:51:27 -0300

    Update densenet_train_all.py

--- bin/densenet_train_all.py
+++ bin/densenet_train_all.py
@@ -1,60 +1,44 @@
 #!/usr/bin/env python
 
 from src.dense_net.train import train_densenet as train
-# import tensorflow as tf
-
-# gpus = tf.config.experimental.list_physical_devices('GPU')
-# if gpus:
-   # Restrict TensorFlow to only use the first GPU
-#   try:
-#     tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
-#     logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-#     print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
-#   except RuntimeError as e:
-     # Visible devices must be set before GPUs have been initialized
-#     print(e)
 
 config = {
-    'data.dataset_name': ['ciarp', 'rwth'], 
-    'data.rotation_range': [0,30], 
+    'data.dataset_name': ['ciarp', 'lsa16', 'rwth'], 
+    'data.test_size': 0.25,
+    'data.train_size': [0.33, 0.5, 0.64, 0.75],
+    'data.rotation_range': [0,30],
     'data.width_shift_range': [0,0.2], 
     'data.height_shift_range': [0,0.2], 
     'data.horizontal_flip': [False,True], 
-    'model.growth_rate': [[32,64],[32,64],[64,128]], 
-    'model.nb_layers': [[6,12,24,16],[6,12,16],[6,12]],
-    'model.reduction': [0.5],
+    'model.growth_rate': [[128,64],[64,64]], 
+    'model.nb_layers': [[[6,12],[6,12]],[[6,12],[6,12]]],
+    'model.reduction': 0.5,
 }
 
-for dataset_name in config['data.dataset_name']:
-    for i in range(2):
-        rotation_range = config['data.rotation_range'][i]
-        width_shift_range = config['data.width_shift_range'][i]
-        height_shift_range = config['data.height_shift_range'][i]
-        horizontal_flip = config['data.horizontal_flip'][i]
-        for i in range(3):
-            nb_layers = config['model.nb_layers'][i]
-            for growth_rate in config['model.growth_rate'][i]:
-                for reduction in config['model.reduction']:
-                    try:
-                        train(dataset_name=dataset_name,rotation_range=rotation_range,
-                              width_shift_range=width_shift_range, height_shift_range= height_shift_range,
-                              horizontal_flip=horizontal_flip,growth_rate=growth_rate,
-                              nb_layers=nb_layers,reduction=reduction, batch_size=16)
-                    except:
-                        try:
-                            train(dataset_name=dataset_name,rotation_range=rotation_range,
-                                  width_shift_range=width_shift_range, height_shift_range= height_shift_range,
-                                  horizontal_flip=horizontal_flip,growth_rate=growth_rate,
-                                  nb_layers=nb_layers,reduction=reduction, batch_size=8)
-                        except:
-                            print("Error with {}, growth: {}, reduction: {}. Probably memory".format(nb_layers, growth_rate, reduction))
-                    finally:
-                        print("Finished densenet with")
-                        print("dataset_name: {}".format(dataset_name))
-                        print("rotation_range: {}".format(rotation_range))
-                        print("width_shift_range: {}".format(width_shift_range))
-                        print("height_shift_range: {}".format(height_shift_range))
-                        print("horizontal_flip: {}".format(horizontal_flip))
-                        print("growth_rate: {}".format(growth_rate))
-                        print("nb_layers: {}".format(nb_layers))
-                        print("reduction: {}".format(reduction))
+for i in range(3):
+    for j in range(2):
+        rotation_range = config['data.rotation_range'][j]
+        width_shift_range = config['data.width_shift_range'][j]
+        height_shift_range = config['data.height_shift_range'][j]
+        horizontal_flip = config['data.horizontal_flip'][j]
+        dataset_name = config['data.dataset_name'][i]
+        growth_rate = config['model.growth_rate'][i][j]
+        nb_layers = config['model.nb_layers'][i][j]
+        reduction = config['model.reduction']
+        test_size = config['data.test_size']
+        for train_size in config['data.train_size']:                 
+            train(dataset_name=dataset_name,rotation_range=rotation_range,
+                    width_shift_range=width_shift_range, height_shift_range= height_shift_range,
+                    horizontal_flip=horizontal_flip,growth_rate=growth_rate,
+                    nb_layers=nb_layers,reduction=reduction, test_size=test_size,
+                    train_size=train_size, batch_size=16)
+            print("Finished densenet with")
+            print("dataset_name: {}".format(dataset_name))
+            print("rotation_range: {}".format(rotation_range))
+            print("width_shift_range: {}".format(width_shift_range))
+            print("height_shift_range: {}".format(height_shift_range))
+            print("horizontal_flip: {}".format(horizontal_flip))
+            print("growth_rate: {}".format(growth_rate))
+            print("nb_layers: {}".format(nb_layers))
+            print("reduction: {}".format(reduction)) 
+            print("train size: {}".format(train_size)) 
